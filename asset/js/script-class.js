4cb51f87a8caebb22c470d51f68a7a87e27c1f96
Penguin92.prg <penguin92.prg@gmail.com>
2026-04-27 22:57:27 +0900

    Update いろいろ追加

--- asset/js/script-class.js
+++ asset/js/script-class.js
@@ -9,6 +9,7 @@ export function loadClass(data){
     cell.innerHTML = "";
   });
 
+  // 各曜限ごとのデータを取得・更新
   for(let dayIndex = 0; dayIndex < 5; dayIndex++){
     for(let periodIndex = 0; periodIndex < 6; periodIndex++){
 
@@ -22,8 +23,11 @@ export function loadClass(data){
       const periodClass = data[dayIndex][periodIndex];
       if(Object.keys(periodClass).length === 0){
         cell.addEventListener("click", function(){
-          this.classList.toggle("active");
-          console.log(this);
+          document.querySelectorAll("#classTable [data-day]").forEach(cell => {
+            cell.classList.remove("active");
+          });
+          this.classList.add("active");
+          classAddConfirm();
         });
         continue;
       }
@@ -50,4 +54,33 @@ export function loadClass(data){
     classIntensiveContainer.appendChild(classIntensiveName);
     document.getElementById("classIntensiveTable").appendChild(classIntensiveContainer);
   }
+
+  // 授業追加用モーダルの閉じるボタンの動作設定
+  document.getElementById("confirmModalClose").addEventListener("click", function(){
+    document.getElementById("confirmModal").classList.remove("active");
+    document.querySelectorAll("#classTable [data-day]").forEach(cell => {
+      cell.classList.remove("active");
+    });
+  });
+}
+
+function classAddConfirm(){
+  const dialog = document.getElementById("confirmModal");
+  const message = dialog.querySelector("p");
+  const btnContainer = dialog.querySelector("#confirmBtnContainer");
+
+  dialog.classList.add("active");
+  message.textContent = "この曜限に授業を登録しますか？"
+
+  const btn1 = document.createElement("button");
+  const btn2 = document.createElement("button");
+  
+  btn1.setAttribute("id", "confirmBtn1");
+  btn2.setAttribute("id", "confirmBtn2");
+  btn1.innerText = "はい";
+  btn2.innerText = "いいえ";
+
+  btnContainer.innerHTML = "";
+  btnContainer.appendChild(btn1);
+  btnContainer.appendChild(btn2);
 }
